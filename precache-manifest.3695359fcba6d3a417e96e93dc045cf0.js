db70d5c194adeef7c5999b1270c6f380dcb5b062
Nguyen Tan Tai <tainguyen.uit@gmail.com>
2020-08-14 12:53:10 +0700

    Updates

---
+++ precache-manifest.3695359fcba6d3a417e96e93dc045cf0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65041034e5816d4ce5cdb1ce381a5b1a",
+    "url": "/simple-counter-app/index.html"
+  },
+  {
+    "revision": "d910248caa23eed32f4c",
+    "url": "/simple-counter-app/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "8f5732caf159115365d7",
+    "url": "/simple-counter-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d910248caa23eed32f4c",
+    "url": "/simple-counter-app/static/js/2.54236e57.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/simple-counter-app/static/js/2.54236e57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f5732caf159115365d7",
+    "url": "/simple-counter-app/static/js/main.f0ddc90f.chunk.js"
+  },
+  {
+    "revision": "0d996f08e1fb04e7d124",
+    "url": "/simple-counter-app/static/js/runtime-main.60afdbf5.js"
+  }
+]);
